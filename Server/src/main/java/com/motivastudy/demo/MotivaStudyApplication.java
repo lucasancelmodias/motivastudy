c10e6c04d8dd8920c547511dd13d559bedd7ebec
Lucas Ancelmo Dias <58889398+lucasancelmo@users.noreply.github.com>
2021-03-21 23:46:49 -0300

    código base angular e java spring boot

---
+++ Server/src/main/java/com/motivastudy/demo/MotivaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.motivastudy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivaStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotivaStudyApplication.class, args);
+	}
+
+}
