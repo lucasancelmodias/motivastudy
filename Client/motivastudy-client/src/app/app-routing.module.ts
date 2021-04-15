33ee47d667aeefc55c6861fe5d75a5b9d0735aad
Rayssa <rayssabanti@hotmail.com>
2021-04-15 19:13:17 -0300

    Estruturação do front - Home/Cadastro/Login

--- Client/motivastudy-client/src/app/app-routing.module.ts
+++ Client/motivastudy-client/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+// Importação dos componentes das páginas 
+import { HomeComponent } from "./pages/home/home.component";
+import { CadastroComponent } from "./pages/cadastro/cadastro.component";
+import { LoginComponent } from "./pages/login/login.component";
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: "cadastro", component: CadastroComponent },
+  { path: "login", component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+  
+  
 export class AppRoutingModule { }
--- Client/motivastudy-client/src/app/app.module.ts
+++ Client/motivastudy-client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FormComponent } from './pages/cadastro/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CadastroComponent,
+    LoginComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
