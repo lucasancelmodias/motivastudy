4cf42fda3f2e3e77944f02f17a6809b9d5621e44
matgomalb <matgomalb@hotmail.com>
2021-05-29 09:00:14 -0300

    Validacoes Front Cadastro/atualizacao front login

--- Client/motivastudy-client/src/app/services/cadastro.service.ts
+++ Client/motivastudy-client/src/app/services/cadastro.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,12 +12,16 @@ constructor(private http: HttpClient) { }
 
 
   cadastrarUsuario(form:any){
+    console.log('FORM >>> ' +form);
     this.http.post(`${environment.url}/cadastro`, form.value)
     .subscribe(
       (response:any) => {
-      console.log('usuário cadastrado');
-    },(error:any) => {
+        alert(response.message); //Response com message do back
+        //Direciona page login
+        window.location.href = '/login';
+    },(error: HttpErrorResponse) => {
       console.error(error.error);
+      alert(error.error.message);
     }
     )
   }
