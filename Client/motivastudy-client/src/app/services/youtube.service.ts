dec340ba540f76c1af6980b5347eaacfe2a22ff2
joaovbds4 <joaosantos.4769@aluno.saojudas.br>
2021-06-02 20:34:26 -0300

    requisição Youtube

--- Client/motivastudy-client/src/app/services/youtube.service.ts
+++ Client/motivastudy-client/src/app/services/youtube.service.ts
@@ -9,21 +9,13 @@ import { Observable } from 'rxjs';
 
 export class YoutubeService {
 
-    apiKey : string = 'AIzaSyBf_w-gztU8dev9LoMXOxtF5GsmKIsMf1I';
+    apiKey : string = 'AIzaSyBz4fTMkjkLWD_eyZFSs2h151DDbIybL6k';
 
     constructor(public http:HttpClient){}
 
-    getVideosForChanel(channel, maxResults): Observable<Object> {
-        let url = 'https://www.googleapis.com/youtube/v3/search?key=' + this.apiKey + '&channelId=' + channel + '&order=date&part=snippet &type=video,id&maxResults=' + maxResults
-        return this.http.get(url)
-          .pipe(map((res) => {
-            return res;
-          }))
-    }
 
-    getVideosPlaylist(playlistId): Observable<Object>{
-        let url = 'https://www.googleapis.com/youtube/v3/search?key=' + this.apiKey + '&playlistId'+ playlistId + '&order=date&part=snippet'
-        
+    getVideosPlaylist(playlistId): Observable<Object>{ 
+        let url ='https://youtube.googleapis.com/youtube/v3/playlistItems?key='+this.apiKey+'&part=snippet%2CcontentDetails&maxResults=25&playlistId='+ playlistId
         return this.http.get(url)
         .pipe(map((res) => {
          return res;
